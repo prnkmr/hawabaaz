2975e456a0e3dbe6798861cdf94672405380f6b4
balakumaranmuthurajan <muthurajanbalakumaran@gmail.com>
2015-08-18 20:08:01 +0530

    with login and register

--- app/src/main/java/praveen/hawabaaz/RegisterActivity.java
+++ app/src/main/java/praveen/hawabaaz/RegisterActivity.java
@@ -4,23 +4,19 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.TextView;
 
-public class MainActivity extends ActionBarActivity {
+public class RegisterActivity extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        TextView tv = (TextView) findViewById(R.id.usrdisplay);
-        Bundle bd = getIntent().getExtras();
-        tv.setText(bd.get("email").toString());
+        setContentView(R.layout.activity_register);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        getMenuInflater().inflate(R.menu.menu_register, menu);
         return true;
     }
 
